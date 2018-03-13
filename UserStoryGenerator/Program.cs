a153dcd34d7c60d1aef511f7d59dd4f128697215
Zsuzsa Rita Bogár <Zsuzsa.Bogar@techtalk.at>
2018-03-13 14:51:41 +0100

    Add project files.

---
+++ UserStoryGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStoryGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
